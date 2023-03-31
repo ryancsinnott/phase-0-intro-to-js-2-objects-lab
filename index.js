392f1d126fcf2345aa18506c8865a162fc3db120
Ryan Sinnott <ryan.christopher.sinnott@gmail.com>
2023-03-30 21:09:49 -0400

    Completed Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+    name: "Ryan",
+    streetAddress: "133 Broadway",
+  };
+
+  function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    const newEmployee = {...employee}
+
+    newEmployee[name] = streetAddress;
+    
+    return newEmployee;
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    employee[name] = streetAddress
+
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(...name) {
+    const employee = {...name}
+    return employee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, name, streetAddress){
+    employee[name] = streetAddress
+
+    return employee;
+  }
